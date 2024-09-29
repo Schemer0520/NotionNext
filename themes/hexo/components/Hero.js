05fc0ca49fb8888a87e38d5d693d82811ffd77c7
Schemer0520 <145528548+Schemer0520@users.noreply.github.com>
2024-09-29 18:25:07 +0800

    Update Hero.js

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -63,7 +63,7 @@ const Hero = props => {
       <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
         <div className='font-black text-4xl md:text-5xl shadow-text'>
-          {siteInfo?.title || siteConfig('TITLE')}
+          你好
         </div>
         {/* 站点欢迎语 */}
         <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
